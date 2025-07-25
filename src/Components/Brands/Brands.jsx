5b4299df5dd7e8d63abdae6c3c60cf1a13fd7960
Eng-gehad24 <153834368+Eng-gehad24@users.noreply.github.com>
2025-07-25 03:51:13 +0300

    Update Brands.jsx

--- src/Components/Brands/Brands.jsx
+++ src/Components/Brands/Brands.jsx
@@ -1,4 +1,4 @@
-import React ,{useState, useEffect}from 'react'
+// import React ,{useState, useEffect}from 'react'
 import { Triangle } from 'react-loader-spinner'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
